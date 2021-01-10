d927a64ebbfe8c15a025421db233493735ab24bf
jimmy <tu_jm@yahoo.com>
2021-01-09 18:01:39 -0800

    Initial Commit

---
+++ slotmaster/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  slotmaster
+//
+//  Created by jimmy on 2021-01-09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
